de3f8dbb94244d2a7243049dc2783a56936158fc
Sidharrth Nagappan <sidharrth2002@gmail.com>
2022-03-03 23:57:55 +0800

    working script with arg parser

--- app.py
+++ app.py
@@ -4,30 +4,47 @@ import json
 import time
 import logging
 
-consumer_key = os.environ.get('TWITTER_CONSUMER_KEY')
-consumer_secret = os.environ.get('TWITTER_CONSUMER_SECRET')
-access_token = os.environ.get('TWITTER_ACCESS_TOKEN')
-access_token_secret = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
+logging.basicConfig(level=logging.INFO)
+
+from decouple import config
+from datetime import datetime, timedelta, timezone
+import datetime
+
+consumer_key = config('TWITTER_CONSUMER_KEY')
+consumer_secret = config('TWITTER_CONSUMER_SECRET')
+access_token = config('TWITTER_ACCESS_TOKEN')
+access_token_secret = config('TWITTER_ACCESS_TOKEN_SECRET')
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
-def save_json(filename, obj, data_dir):
+def save_list_to_json(filename, obj, data_dir):
     # Writing to JSON
     with open(os.path.join(data_dir, filename), 'w') as f:
-        f.write(json.dumps([tweet._json for tweet in obj], indent=4))
+        f.write(json.dumps([dict(tweet._json) for tweet in obj], indent=4))
 
-def get_replies(tweet_id, user_name, data_dir='./data'):
+def save_obj_to_json(filename, obj, data_dir):
+    # Writing to JSON
+    with open(os.path.join(data_dir, filename), 'w') as f:
+        f.write(json.dumps(obj, indent=4))
+
+def get_replies(tweets, tweet_ids, user_name, data_dir='./data'):
+    latest_tweet_id = tweet_ids[-1] + 1
     replies = tweepy.Cursor(api.search_tweets, q='to:{}'.format(user_name),
-                                    since_id=tweet_id, tweet_mode='extended').items()
+                                    since_id=latest_tweet_id, tweet_mode='extended', count=200).items()
     all_replies = []
     while True:
         try:
             reply = replies.next()
             if not hasattr(reply, 'in_reply_to_status_id_str'):
                 continue
-            if reply.in_reply_to_status_id == tweet_id:
+            tweet_id = reply.in_reply_to_status_id
+            if reply.in_reply_to_status_id in tweet_ids:
+                index = tweet_ids.index(tweet_id)
+                if 'replies' not in tweets[index]._json:
+                    tweets[index]._json['replies'] = []
+                tweets[index]._json['replies'].append(reply._json)
                 logging.info("reply of tweet:{}".format(reply.full_text))
                 all_replies.append(reply)
 
@@ -51,22 +68,20 @@ def get_replies(tweet_id, user_name, data_dir='./data'):
             logging.error("Failed while fetching replies {}".format(e))
             break
 
-    return all_replies
+    return tweets
 
-def downloadTimeline(screen_name, count, startDate, endDate):
-  counter = 0
+def downloadTimeline(screen_name, startDate, endDate, data_dir, count=200):
+  logging.info(f"Downloading timeline for {screen_name} from {startDate} to {endDate}")
+  startDate = datetime.datetime(int(startDate.split('-')[0]), int(startDate.split('-')[1]), int(startDate.split('-')[2]), tzinfo=timezone(offset=timedelta()))
+  endDate = datetime.datetime(int(endDate.split('-')[0]), int(endDate.split('-')[1]), int(endDate.split('-')[2]), tzinfo=timezone(offset=timedelta()))
   results = api.user_timeline(screen_name=screen_name, count=count, include_rts=True, exclude_replies=False)
   tweets = []
-  replies = {}
+  tweet_ids = []
   for tweet in results:
-    tweets.append(tweet)
-    tweet_replies = get_replies(tweet.id, screen_name)
-    replies[tweet.id] = tweet_replies
-  save_json(f'{screen_name}_timeline_{counter}.json', tweets)
-  save_json(f'{screen_name}_replies_{counter}.json', replies)
-  tweets = []
-  replies = {}
-  counter += 1
+    if (tweet.created_at >= startDate) and (tweet.created_at <= endDate):
+        tweets.append(tweet)
+        tweet_ids.append(tweet.id)
+        logging.info(f"Tweet: {tweet.text}")
 
   while (results[-1].created_at > startDate):
     logging.info(f'Last tweet collected was created at {results[-1].created_at}, retrieving more')
@@ -74,14 +89,17 @@ def downloadTimeline(screen_name, count, startDate, endDate):
     for tweet in results:
       if (tweet.created_at >= startDate):
         tweets.append(tweet)
-        tweet_replies = get_replies(tweet.id, screen_name)
-        replies[tweet.id] = tweet_replies
+        tweet_ids.append(tweet.id)
 
-    save_json(f'{screen_name}_timeline_{counter}.json', tweets)
-    save_json(f'{screen_name}_replies_{counter}.json', replies)
-    tweets = []
-    replies = {}
-    counter += 1
+  # save only tweets without replies
+  save_list_to_json(f'{screen_name}_tweets.json', tweets, data_dir)
 
-  for tweet in tweets:
-    print(f'{tweet.id} | {tweet.text}')
+  logging.info("Saved all tweets")
+
+  # get replies
+  tweets_with_replies = get_replies(tweets, tweet_ids, screen_name, data_dir)
+
+  # save tweets with replies
+  save_list_to_json(f'{screen_name}_tweets_replies.json', tweets_with_replies, data_dir)
+
+  logging.info("Saved all tweets with replies")
--- run.py
+++ run.py
@@ -6,7 +6,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--screen_name", help="Twitter screen name", required=True)
 parser.add_argument("--start_date", help="Start date in YYYY-MM-DD format", required=True)
 parser.add_argument("--end_date", help="End date in YYYY-MM-DD format", required=True)
-parser.add_argument("--data_dir", default="./data", help="Directory to save data", required=True)
+parser.add_argument("--data_dir", help="Directory to save data")
 
 args = parser.parse_args()
 
