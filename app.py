8cdc45ef597ee00ddae65eae44aeeaf0c580f0f7
Sidharrth Nagappan <sidharrth2002@gmail.com>
2022-03-04 00:26:07 +0800

    add sleep

--- app.py
+++ app.py
@@ -15,6 +15,9 @@ consumer_secret = config('TWITTER_CONSUMER_SECRET')
 access_token = config('TWITTER_ACCESS_TOKEN')
 access_token_secret = config('TWITTER_ACCESS_TOKEN_SECRET')
 
+now = datetime.datetime.now()
+current_date_time = now.strftime("%m_%d_%Y-%H_%M_%S")
+
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True)
@@ -34,8 +37,14 @@ def get_replies(tweets, tweet_ids, user_name, data_dir='./data'):
     replies = tweepy.Cursor(api.search_tweets, q='to:{}'.format(user_name),
                                     since_id=latest_tweet_id, tweet_mode='extended', count=200).items()
     all_replies = []
+
+    i = 0
+    count = 200
+    sleep_secs = 4  # Implementing sleep due to Twitter's rate limit, 5s seems to be the perfect delay
+
     while True:
         try:
+            i += 1
             reply = replies.next()
             if not hasattr(reply, 'in_reply_to_status_id_str'):
                 continue
@@ -47,6 +56,9 @@ def get_replies(tweets, tweet_ids, user_name, data_dir='./data'):
                 tweets[index]._json['replies'].append(reply._json)
                 logging.info("reply of tweet:{}".format(reply.full_text))
                 all_replies.append(reply)
+            if (i % count == 0):
+                print(f'Iterated #{i} status Tweets, collected {len(replies)} Tweet replies so far, sleeping for {sleep_secs} seconds')
+                time.sleep(sleep_secs)
 
         except tweepy.TooManyRequests as e:
             logging.error("Twitter api rate limit reached".format(e))
@@ -103,3 +115,54 @@ def downloadTimeline(screen_name, startDate, endDate, data_dir, count=200):
   save_list_to_json(f'{screen_name}_tweets_replies.json', tweets_with_replies, data_dir)
 
   logging.info("Saved all tweets with replies")
+
+  mentioned_tweets_count = downloadMentionedTweets(screen_name, tweets_with_replies[-1].id, data_dir, startDate, endDate)
+
+  logging.info("Saved all mentioned tweets")
+
+  return tweets_with_replies
+
+def downloadMentionedTweets(screen_name, tweet_id, data_dir, startDate, endDate, count=200):
+    '''Function which returns the mentioned Tweets of a particular brand, given a specific start date'''
+    mentionedTweets = []
+    i = 0  # Pausing on every 200 Tweets (maximum count for Tweet retrievable)
+    sleep_secs = 4
+    # 180 requests for every 15 minutes, which brings to 5 seconds for every request, which means that it's more effective to sleep 4 seconds, under the assumption there is a 1s processing time for every request
+    # Not much justification towards why choosing this sleep value, but found that it works quite well
+    # Works quite well
+    save_filename = f'saving_mentioned_tweets_{current_date_time}.json'
+    save_filepath = os.path.join(data_dir, save_filename)
+
+    if save_filename not in os.listdir(data_dir):
+        # If Tweets json file not available, create the data file
+        with open(save_filepath, 'w') as f:
+            f.write('')
+
+    for status in tweepy.Cursor(api.search_tweets, q='@{}'.format(screen_name), since_id=tweet_id, tweet_mode='extended', count=count).items():
+        mentionedTweets.append(status)
+        i += 1
+
+        if (status.created_at > endDate):
+            # Limit retrieving Tweets only start with since Tweet ID, and end at a specific date
+            break
+
+        if (i % count == 0):
+            # Sleep for every 100 Tweets retrieved, due to Twitter API's rate limits
+            time.sleep(sleep_secs)
+            print(f'Added mentioned Tweet #{i}, sleeping for {sleep_secs} seconds')
+
+        if (i % 10000 == 0):
+            # Write to file for every 10k JSON objects, then clear array, saves memory this way
+            with open(save_filepath, 'a') as f:
+                for t in mentionedTweets:
+                    if (t.created_at >= startDate) and (t.created_at <= endDate):
+                        # Only limit by start and end date ranges
+                        f.write(json.dumps(t._json) + '\n')
+            mentionedTweets = []
+
+    with open(save_filepath, 'a') as f:
+        # Write remaining Tweets to file again at the end, before returning
+        for t in mentionedTweets:
+            if (t.created_at >= startDate) and (t.created_at <= endDate):
+                f.write(json.dumps(t._json) + '\n')
+    return i
