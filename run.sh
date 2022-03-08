e51c705bdf0dcffe2e60bc3c280ad14e570f49cc
Sidharrth Nagappan <sidharrth2002@gmail.com>
2022-03-08 17:06:56 +0800

    use lpnational

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python3 run.py --screen_name "TheDemocrats" --start_date "2022-02-24" --end_date "2022-02-26" --data_dir "./data"
+python3 run.py --screen_name "LPNational" --start_date "2022-02-24" --end_date "2022-02-26" --data_dir "./data"
